17a53bb85063e3702cf7c480231a4216f9756155
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-16 20:15:25 +0000

    chore: maintenance updates

--- src/OfficeForge/Models/PresentationModel.cs
+++ src/OfficeForge/Models/PresentationModel.cs
@@ -1,7 +1,23 @@
 namespace OfficeForge.Models;
 
-public sealed class PresentationModel : IPresentationModel
+public sealed class PresentationModel : IPresentationModel, IEquatable<PresentationModel>
 {
+    public bool Equals(PresentationModel? other)
+    {
+        return other != null &&
+               EqualityComparer<List<SlideModel>>.Default.Equals(Slides, other.Slides);
+    }
+
+    public override bool Equals(object? obj) => ReferenceEquals(this, obj) || obj is PresentationModel other && Equals(other);
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Slides);
+    }
+
+    public static bool operator ==(PresentationModel? left, PresentationModel? right) => EqualityComparer<PresentationModel>.Default.Equals(left, right);
+
+    public static bool operator !=(PresentationModel? left, PresentationModel? right) => !(left == right);
     public List<SlideModel> Slides { get; } = [];
 
     public SlideModel AddSlide(string? title = null)
